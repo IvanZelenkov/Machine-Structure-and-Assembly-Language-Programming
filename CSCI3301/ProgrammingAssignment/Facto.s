0c15527ebb7132e4c397df1f2fc6615019eb6322
Ivan Zelenkov <64171964+IvanZelenkov@users.noreply.github.com>
2022-05-07 18:25:43 -0500

    Update Facto.s

--- CSCI3301/ProgrammingAssignment/Facto.s
+++ CSCI3301/ProgrammingAssignment/Facto.s
@@ -2,7 +2,8 @@
 # Author: Ivan Zelenkov
 # Creation Date: 05/01/2022
 
-.data # .data is the section where all variables are located
+# .data is the section where all variables are located
+.data
 
 # .align is used for the arrangement of data in memory, and specifically deals with 
 # the issue of accessing data as proper units of information from main memory
@@ -20,36 +21,36 @@ BoundaryMessage: .asciiz "Error: Result is larger than 32 bits\n"
 
 main:
 	li $v0, 4             # print function name
-    la $a0, FunctionName
-    syscall
+	la $a0, FunctionName
+	syscall
     
-    li $v0, 4             # print a message for the user
-    la $a0, Enter
-    syscall
+    	li $v0, 4             # print a message for the user
+    	la $a0, Enter
+    	syscall
 
-    li $v0, 5             # get input value n
-    syscall
+    	li $v0, 5             # get input value n
+	syscall
     
-    add $a0, $v0, $zero   # pass argument to factorial function
-    jal factorial
-    
-    li $v0, 4             # print result for the user
-    la $a0, Result
-    syscall
+	add $a0, $v0, $zero   # pass argument to factorial function
+	jal factorial
 
-    li $v0, 1             # print result
-    add $a0, $v1, $zero
-    syscall
+	li $v0, 4             # print result for the user
+	la $a0, Result
+	syscall
 
-    j exit
+	li $v0, 1             # print result
+	add $a0, $v1, $zero
+	syscall
+
+	j exit
 
 factorial:
 	addi $sp, $sp, -8     # adjust stack for 2 items
 	sw   $ra, 4($sp)      # save return address
 	sw   $a0, 0($sp)      # save argument (~push)
-	slti $t0, $a0, 0	  # test for n < 0
+	slti $t0, $a0, 0      # test for n < 0
 	beq  $t0, 1, NegativeNumberException 
-	addi $t1, $t1, 12	  # $t1 = $t1 + 12
+	addi $t1, $t1, 12     # $t1 = $t1 + 12
 	bgt  $a0, $t1, BoundaryException # test for n > 12
 	slti $t0, $a0, 1      # test for n < 1, where n = 0
 	beq  $t0, $zero, else
@@ -68,18 +69,18 @@ else:
 	
 NegativeNumberException:
 	li $v0, 4             # print function name
-    la $a0, NegativeNumberMessage
-    syscall
+    	la $a0, NegativeNumberMessage
+    	syscall
     
-    j exit
+    	j exit
     
 BoundaryException:
 	li $v0, 4             # print function name
-    la $a0, BoundaryMessage
-    syscall
+	la $a0, BoundaryMessage
+	syscall
     
    	j exit
     
 exit: 
 	li $v0, 10            # exit
-    syscall
+    	syscall
